d68c40f5dd032b2550cca6092a21192fdc3846dc
ThanatosDrive <hora.gabor.daniel@gmail.com>
2025-10-15 18:30:22 +0200

    incremented version counter
    incremented version counter

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="jsonxplode",
-    version="1.2.0",
+    version="1.2.1",
     author="ThanatosDrive",
     description="Efficient JSON flattening for complex nested structures",
     long_description=long_description,
